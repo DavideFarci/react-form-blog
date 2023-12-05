16cf4fb989cecc846c06ca2a91e4d0a5ec0f795e
DavideFarci <yank92.df@gmail.com>
2023-12-05 17:51:25 +0100

    add font-awesome, refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,44 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
-function App() {
+const initialData = {
+  id: '',
+  title: '',
+};
+
+const App = () => {
   const [postList, setPostList] = useState([]);
-  const initialData = {
-    id: '',
-    title: '',
-  };
-
   const [formValues, setFormValues] = useState(initialData);
   const [editValues, setEditValues] = useState(initialData);
   const [toggleEdit, setToggleEdit] = useState(false);
 
-  const createPost = (newValue, fieldName) => {
-    const newFormValue = { ...formValues };
-
-    newFormValue[fieldName] = newValue;
-
-    setFormValues(newFormValue);
+  const handleChange = (e, values, setValues, fieldName) => {
+    const newValues = { ...values };
+    newValues[fieldName] = e.target.value;
+    setValues(newValues);
   };
 
-  const updatePost = (newValue, fieldName) => {
-    const newEditValue = { ...editValues };
+  const createPost = (e) => {
+    e.preventDefault();
+    setPostList([...postList, { ...formValues, id: crypto.randomUUID() }]);
+    setFormValues(initialData);
+  };
 
-    newEditValue[fieldName] = newValue;
-
-    setEditValues(newEditValue);
+  const updatePost = (e) => {
+    e.preventDefault();
+    setPostList(
+      postList.map((post) => (post.id === editValues.id ? editValues : post)),
+    );
+    setToggleEdit(false);
+    setEditValues(initialData);
   };
 
   const openFormAndSetValueEdit = (postToUpdate) => {
     setToggleEdit(true);
-
     setEditValues(postToUpdate);
-
-    // setToggleEdit(false);
   };
 
-  const removePost = (postToRemove) => {
-    setPostList(postList.filter((post) => post.id !== postToRemove));
-  };
-
-  // Funzione per submit del form
-  const handleFormCreateSubmit = (e) => {
-    // Per evitare il refresh
-    e.preventDefault();
-
-    setPostList([
-      ...postList,
-      {
-        ...formValues,
-        id: crypto.randomUUID(),
-        title: formValues.title,
-      },
-    ]);
-
-    // reset input
-    setFormValues(initialData);
-  };
-
-  const handleFormEditSubmit = (e) => {
-    e.preventDefault();
-
-    setPostList(
-      postList.map((post) => (post.id == editValues.id ? editValues : post)),
-    );
-
-    setToggleEdit(false);
-
-    setEditValues(initialData);
+  const removePost = (postId) => {
+    setPostList(postList.filter((post) => post.id !== postId));
   };
 
   return (
@@ -74,16 +46,13 @@ function App() {
       <h1 className="py-8 text-center font-mono text-3xl font-bold">
         Post Create
       </h1>
-      <form
-        onSubmit={handleFormCreateSubmit}
-        className="mx-auto flex max-w-xl flex-col"
-      >
+      <form onSubmit={createPost} className="mx-auto flex max-w-xl flex-col">
         <label htmlFor="title" className="font-semibold">
           Titolo del post
         </label>
         <input
           value={formValues.title}
-          onChange={(e) => createPost(e.target.value, 'title')}
+          onChange={(e) => handleChange(e, formValues, setFormValues, 'title')}
           type="text"
           name="title"
           className="mb-2 w-full rounded-md border-2 p-2 text-black"
@@ -98,9 +67,8 @@ function App() {
       </form>
 
       <h2 className="px-8 py-8 font-mono text-2xl font-bold">Posts List</h2>
-      {/* Form per modifica post  */}
       <form
-        onSubmit={handleFormEditSubmit}
+        onSubmit={updatePost}
         className={`${!toggleEdit && 'hidden'} px-8 py-8`}
       >
         <label htmlFor="title" className="font-semibold">
@@ -108,7 +76,7 @@ function App() {
         </label>
         <input
           value={editValues.title}
-          onChange={(e) => updatePost(e.target.value, 'title')}
+          onChange={(e) => handleChange(e, editValues, setEditValues, 'title')}
           type="text"
           name="title"
           className="mb-2 w-full rounded-md border-2 p-2 text-black"
@@ -122,34 +90,30 @@ function App() {
         </button>
       </form>
       <ul className="max-w-2xl">
-        {postList.map((post, i) => {
-          return (
-            <li className="mb-3 flex justify-between pl-4 pr-40" key={post.id}>
-              <span>
-                <span className="mr-4 font-bold">{i}.</span> {post.title}
+        {postList.map((post, i) => (
+          <li className="mb-3 flex justify-between pl-4 pr-40" key={post.id}>
+            <span>
+              <span className="mr-4 font-bold">{i + 1}.</span> {post.title}
+            </span>
+            <div>
+              <span
+                onClick={() => openFormAndSetValueEdit(post)}
+                className="mr-4 rounded-md bg-orange-400 px-2 py-1 text-sm font-semibold shadow-sm shadow-orange-500 duration-200 hover:cursor-pointer hover:bg-orange-600 hover:px-4"
+              >
+                Edit
               </span>
-              <div>
-                {/* Bottone per aprire form di edit  */}
-                <span
-                  onClick={() => openFormAndSetValueEdit(post)}
-                  className="mr-4 rounded-md bg-orange-400 px-2 py-1 text-sm font-semibold shadow-sm shadow-orange-500 duration-200 hover:cursor-pointer hover:bg-orange-600 hover:px-4"
-                >
-                  Edit
-                </span>
-                {/* Cancellazione del post  */}
-                <span
-                  onClick={() => removePost(post.id)}
-                  className="font-bold hover:cursor-pointer"
-                >
-                  X
-                </span>
-              </div>
-            </li>
-          );
-        })}
+              <span
+                onClick={() => removePost(post.id)}
+                className="font-bold hover:cursor-pointer"
+              >
+                <i className="fa-solid fa-trash-can duration-200 hover:text-orange-600"></i>
+              </span>
+            </div>
+          </li>
+        ))}
       </ul>
     </>
   );
-}
+};
 
 export default App;
